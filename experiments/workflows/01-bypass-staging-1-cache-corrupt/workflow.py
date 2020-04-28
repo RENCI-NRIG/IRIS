04711996973d53a21eb2737203073e69c1b462af
Ryan Tanaka <tanaka@isi.edu>
2020-04-28 14:30:56 -0700

    refactor bypass wf

--- experiments/workflows/01-bypass-staging-1-cache-corrupt/workflow.py
+++ experiments/workflows/01-bypass-staging-1-cache-corrupt/workflow.py
@@ -2,15 +2,73 @@
 
 import getpass
 import hashlib
+import logging
 import os
+import subprocess
+
+################################################################################
+######### using IRIS/experiments/common and pegasus-config setup ###############
+sys.path.append(str(Path(__file__).parent.parent.parent.resolve() / "common"))
+
+import util
+import props
+import sites
+
+sys.path.append(util.pegasus_config_python())
+################################################################################
 
 from Pegasus.api import *
 
+logging.basicConfig(level=logging.DEBUG)
 
 def sha256(fname):
     with open(fname,"rb") as f:
         bytes = f.read()
         return hashlib.sha256(bytes).hexdigest()
+
+# --- Work Directory Setup -----------------------------------------------------
+BASE_DIR = Path(__file__).parent.resolve()
+
+RUN_ID=BASE_DIR.name # cwd name
+WORK_DIR = os.getenv("EXPERIMENT_WORK_DIR")
+initiated_by_run_script = WORK_DIR
+
+if not initiated_by_run_script:
+    WORK_DIR = Path(os.getenv("HOME")) / "workflow-runs"
+    RUN_ID += "-" + datetime.now().strftime("%s")
+
+    try:
+        Path.mkdir(WORK_DIR)
+    except FileExistsError:
+        pass
+
+    WORK_DIR = str(WORK_DIR)
+
+# --- Cleanup Caches -----------------------------------------------------------
+util.clear_caches("syr-compute-c2", "unl-compute-c1", "ucsd-compute-c3")
+
+# --- Place Data at Staging Site -----------------------------------------------
+stage = subprocess.run(["ssh", "uc-staging", "mkdir", "-p", "~/public_html/inputs"])
+
+if stage.returncode != 0:
+    raise RuntimeError()
+
+scp = subprocess.run(["scp", str(BASE_DIR / "job-wrapper.sh", "inputs/*", "uc-staging:~/public_html/inputs/")])
+if scp.returncode != 0:
+    raise RuntimeError()
+
+# --- Properties ---------------------------------------------------------------
+props = Properties()
+props["pegasus.data.configuration"] = "nonsharedfs"
+props["pegasus.transfer.bypass.input.staging"] = "True"
+props["pegasus.monitord.encoding"] = "json"
+props["pegasus.catalog.workflow.amqp.url"] = "amqp://friend:donatedata@msgs.pegasus.isi.edu:5672/prod/workflows"
+props["dagman.retry"] = "0"
+props["pegasus.transfer.arguments"] = "-m 1"
+props.write(str(BASE_DIR / "pegasus.properties"))
+
+# --- Sites --------------------------------------------------------------------
+sites.write_basic_site_catalog(str(BASE_DIR / "sites.yml"), WORK_DIR, RUN_ID)
 
 # used all over the place
 username = getpass.getuser()
@@ -47,6 +105,20 @@ for i in range(100):
 
 wf.add_transformation_catalog(tc)
 wf.add_replica_catalog(rc)
-wf.write('workflow.yml')
 
+# start driver experiment
+iris_experiment_driver = subprocess.Popen([str(BASE_DIR / "iris-experiment-driver")])
 
+# start workflow
+wf.plan(
+        verbose=3,
+        output_site="local", 
+        dir=WORK_DIR, 
+        relative_dir=RUN_ID, 
+        sites=["condorpool"], 
+        staging_sites={"condorpool": "origin"}
+)\
+.wait()
+
+# terminate driver 
+iris_experiment_driver.terminate()
