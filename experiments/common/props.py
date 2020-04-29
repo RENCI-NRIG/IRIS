3e63ab073e0996145b3c9e98fbb4b0d86cad5a18
ryan <ryanyt@hawaii.edu>
2020-04-29 16:57:07 +0000

    set dagman.retry=2 in commpon pros

--- experiments/common/props.py
+++ experiments/common/props.py
@@ -7,6 +7,6 @@ def write_basic_properties(path: str) -> None:
     props["pegasus.data.configuration"] = "nonsharedfs"
     props["pegasus.monitord.encoding"] = "json"
     props["pegasus.catalog.workflow.amqp.url"] = "amqp://friend:donatedata@msgs.pegasus.isi.edu:5672/prod/workflows"
-    props["dagman.retry"] = "0"
+    props["dagman.retry"] = "2"
     props.write(path)
     
